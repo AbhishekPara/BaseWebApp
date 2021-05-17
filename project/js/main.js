e89c7dafc34f545573703fd2736cbd1f04f99a78
aparapur <aparapur@uwaterloo.ca>
2021-05-17 12:41:27 -0400

    firebase

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,73 @@
+function addMessage(postTitle,postBody){
+  var postData={
+    title:postTitle,
+    body:postBody
+  }
+  var database = firebase.database().ref("posts");
+  var newPostRef = database.push();
+  newPostRef.set(postData);
+}
+function handleMessageFormSubmit(){
+  var postTitle=$("#post-title").val();
+  var postBody=$("#post-body").val();
+  console.log(postTitle);
+
+  addMessage(postTitle,postBody);
+}
+
+function handleSignIn(){
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.email);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
+
+$(document).ready(function(){
+  getWeather();
+})
+
+function getWeather(searchQuery) {
+  var url="https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text("");
+  $(".error-message").text("");
+  
+  $.ajax(url,{success: function(data){
+    //console.log(data);
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
+}
+
+function searchWeather(){
+  var searchQuery=$(".search").val();
+  getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
@@ -7,3 +77,4 @@ function showPicture(){
   // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
   
 }
+
